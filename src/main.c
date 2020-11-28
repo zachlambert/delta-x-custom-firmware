585fee9c5d09a03963fd21afd34c74212b5063b6
Zach Lambert <zachalambert@gmail.com>
2020-11-28 10:59:47 +0000

    Comparing two implementations

--- src/main.c
+++ src/main.c
@@ -1,70 +1,31 @@
+#define TEST3
+
+// Program: 544
+// Program with inline void setup: 340
+// Data: 8
+#ifdef TEST1
 #include <util/delay.h>
 #include <avr/io.h>
 
-#define TEST4
-
-// Program: 306
-// Data: 0
-#ifdef TEST1
-int main(void)
-{
-    PORTB |= 1<<7;
-    DDRB |= 1<<7;
-    while (1) {
-        PORTB |= 1<<7;
-        _delay_ms(500);
-        PORTB &= ~(1<<7);
-        _delay_ms(500);
-    }
-    return 0;
-}
-#endif
-
-// Program: 340
-// Data: 8
-#ifdef TEST2
 volatile uint8_t *const PORTx[] = {&PORTA, &PORTB};
 volatile uint8_t *const DDRx[] = {&DDRA, &DDRB};
 typedef enum {
     PORT_A,
-    PORT_B
+    PORT_B,
+    PORT_COUNT
 } Port;
 typedef struct {
     uint8_t port: 4;
-    uint8_t n: 4;
-} Pin;
-int main(void)
-{
-    const Pin led = { .port=PORT_A, .n=7 };
-    *PORTx[led.port] |= 1<<led.n;
-    *DDRx[led.port] |= 1<<led.n;
-    while (1) {
-        *PORTx[led.port] |= 1<<led.n;
-        _delay_ms(500);
-        *PORTx[led.port] &= ~(1<<led.n);
-        _delay_ms(500);
-    }
-    return 0;
-}
-#endif
+    uint8_t n: 3;
+} PinImpl;
+typedef const PinImpl *const Pin;
 
-// Program: 540
-// Program with inline void setup: 340
-// Data: 8
-#ifdef TEST3
+#define PIN_B7 { .port=PORT_A, .n=7 }
+#define MAKE_PIN(NAME, PIN) \
+    const PinImpl _NAME = PIN; \
+    Pin NAME = &_NAME;
 
-volatile uint8_t *const PORTx[] = {&PORTA, &PORTB};
-volatile uint8_t *const DDRx[] = {&DDRA, &DDRB};
-typedef enum {
-    PORT_A,
-    PORT_B
-} Port;
-typedef struct {
-    uint8_t port: 4;
-    uint8_t n: 4;
-} Pin;
-
-void setup(const Pin *pin)
+void setup(const Pin pin)
 {
     *PORTx[pin->port] |= 1<<pin->n;
     *DDRx[pin->port] |= 1<<pin->n;
@@ -72,48 +33,126 @@ void setup(const Pin *pin)
 
 int main(void)
 {
-    const Pin led = { .port=PORT_A, .n=7 };
-    setup(&led);
+    MAKE_PIN(led, PIN_B7);
+    setup(led);
     while (1) {
-        *PORTx[led.port] |= 1<<led.n;
+        *PORTx[led->port] |= 1<<led->n;
         _delay_ms(500);
-        *PORTx[led.port] &= ~(1<<led.n);
+        *PORTx[led->port] &= ~(1<<led->n);
         _delay_ms(500);
     }
     return 0;
 }
 #endif
 
-// Program: 410
-// Program with inline void setup: 340
-// Data: 8
-#ifdef TEST4
+// Without inline:
+//   Program: 438
+//   Data: 12
+// With inline:
+//   Program: 344
+//   Data: 12
+#ifdef TEST2
+#include <util/delay.h>
+#include <avr/io.h>
 
 volatile uint8_t *const PORTx[] = {&PORTA, &PORTB};
 volatile uint8_t *const DDRx[] = {&DDRA, &DDRB};
 typedef enum {
     PORT_A,
-    PORT_B
+    PORT_B,
+    PORT_COUNT
 } Port;
 typedef struct {
     uint8_t port: 4;
-    uint8_t n: 4;
-} Pin;
+    uint8_t n: 3;
+} PinImpl;
+typedef const PinImpl *const Pin;
+
+#define PIN_B7 { .port=PORT_A, .n=7 }
+#define MAKE_PIN(NAME, PIN) \
+    const PinImpl _NAME = PIN; \
+    Pin NAME = &_NAME;
 
 inline void setup(const Pin pin)
 {
-    *PORTx[pin.port] |= 1<<pin.n;
-    *DDRx[pin.port] |= 1<<pin.n;
+    *PORTx[pin->port] |= 1<<pin->n;
+    *DDRx[pin->port] |= 1<<pin->n;
+}
+
+MAKE_PIN(led, PIN_B7);
+int main(void)
+{
+    setup(led);
+    while (1) {
+        *PORTx[led->port] |= 1<<led->n;
+        _delay_ms(500);
+        *PORTx[led->port] &= ~(1<<led->n);
+        _delay_ms(500);
+    }
+    return 0;
+}
+#endif
+
+// Without inline:
+//   Program: 544
+//   Data: 8
+// With inline:
+//   Program: 340
+//   Data: 8
+#ifdef TEST3
+#include <util/delay.h>
+#include <avr/io.h>
+
+volatile uint8_t *const PORTx[] = {
+    &PORTA, &PORTB, &PORTC, &PORTD, &PORTE, &PORTF, &PORTG, &PORTH,
+    &PORTJ, &PORTK, &PORTL
+};
+volatile uint8_t *const DDRx[] = {
+    &DDRA, &DDRB, &DDRC, &DDRD, &DDRE, &DDRF, &DDRG, &DDRH,
+    &DDRJ, &DDRK, &DDRL
+};
+
+typedef enum {
+    PORT_A,
+    PORT_B,
+    PORT_C,
+    PORT_D,
+    PORT_E,
+    PORT_F,
+    PORT_G,
+    PORT_H,
+    PORT_J,
+    PORT_K,
+    PORT_L,
+    PORT_COUNT
+} Port;
+
+typedef struct {
+    uint8_t port: 4;
+    uint8_t n: 3;
+} PinImpl;
+
+typedef const PinImpl *const Pin;
+
+#define PIN_B7 { .port=PORT_A, .n=7 }
+#define MAKE_PIN(NAME, PIN) \
+    const PinImpl _NAME = PIN; \
+    Pin NAME = &_NAME;
+
+void setup(const Pin pin)
+{
+    *PORTx[pin->port] |= 1<<pin->n;
+    *DDRx[pin->port] |= 1<<pin->n;
 }
 
 int main(void)
 {
-    const Pin led = { .port=PORT_A, .n=7 };
+    MAKE_PIN(led, PIN_B7);
     setup(led);
     while (1) {
-        *PORTx[led.port] |= 1<<led.n;
+        *PORTx[led->port] |= 1<<led->n;
         _delay_ms(500);
-        *PORTx[led.port] &= ~(1<<led.n);
+        *PORTx[led->port] &= ~(1<<led->n);
         _delay_ms(500);
     }
     return 0;
