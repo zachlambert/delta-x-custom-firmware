7d12418d9c5d5615a39c0e694efc5501c4e3c55c
Zach Lambert <zachalambert@gmail.com>
2021-05-25 15:47:23 +0100

    Added buffer back to main

--- src/main.c
+++ src/main.c
@@ -4,20 +4,65 @@
 #include <string.h>
 #include <util/delay.h>
 
+#define BUFFER_N 14
+typedef struct {
+    uint8_t data[BUFFER_N];
+    int head;
+    uint16_t special;
+    uint16_t special2;
+} Buffer;
+
+Buffer buffer_create(void)
+{
+    Buffer buffer;
+    memset(buffer.data, 0, BUFFER_N);
+    buffer.head = 0;
+    buffer.special = 0xF770;
+    buffer.special2 = 0xE661;
+    return buffer;
+}
+
+void buffer_update(Buffer *buffer)
+{
+    buffer->data[buffer->head] = uart_read_byte();
+    buffer->head = (buffer->head+1)%BUFFER_N;
+}
+
+bool buffer_match(Buffer *buffer)
+{
+    int index = buffer->head;
+    int index2 = (buffer->head+1)%BUFFER_N;
+    uint16_t start = buffer->data[index2]<<8 | buffer->data[index];
+    if (start != buffer->special) return false;
+    index = (buffer->head-2)%BUFFER_N;
+    index2 = (buffer->head-1)%BUFFER_N;
+    uint16_t end = buffer->data[index2]<<8 | buffer->data[index];
+    if (end != buffer->special2) return false;
+    return true;
+}
+
+uint16_t buffer_read_value(Buffer *buffer, int i)
+{
+    int index = (buffer->head+2 + 2*i)%BUFFER_N;
+    int index2 = (index+1)%BUFFER_N;
+    return buffer->data[index2]<<8 | buffer->data[index];
+}
+
 int main(void)
 {
     UartConfig uart_config = uart_create_config();
     uart_init(&uart_config);
 
-    _delay_ms(1000);
+    Buffer buffer = buffer_create();
 
-    uint8_t buffer[10];
-    uart_read_bytes(buffer, 10);
+    _delay_ms(1000);
+    for (int j = 0; j< 20; j++) {
+        buffer_update(&buffer);
+        if (buffer_match(&buffer)) break;
+    }
 
     while (1) {
-        for (int i = 0; i < 10; i++) {
-            printf("0x%x\n", buffer[i]);
-        }
+        printf("Starting\n");
     }
 
 
